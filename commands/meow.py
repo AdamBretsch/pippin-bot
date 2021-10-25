2eb2392e7f5da30adcc595880618b0d7b96559f9
Adam Bretsch <adambretsch@gmail.com>
2021-10-24 23:13:14 -0500

    Cleaned up files

--- commands/meow.py
+++ commands/meow.py
@@ -29,18 +29,10 @@ class Meow(BaseCommand):
         # parameters as specified in __init__
         # 'message' is the discord.py Message object for the command to handle
         # 'client' is the bot Client object
-        
-        #ctx = arguments[1]
-        #channel = ctx.author.voice.channel
-        #vc = await channel.connect()
-        #await ctx.send('Started playing: something')
-        #vc.play(discord.FFmpegPCMAudio('~/pippin_bot/sounds/p_meow1.mp3'), after=lambda e: print('done', e))
         voice_channel = client.get_channel(782744182497738812)
         print("Voice channel got")
         voice_client = await voice_channel.connect()
         print("Voice client connected")
-        voice_client.play(FFmpegPCMAudio('./sounds/p_meow1.mp3'), after=lambda e: await voice_client)
+        voice_client.play(FFmpegPCMAudio('./sounds/p_meow2.mp3'), after=lambda e: print("Meow sent!"))
         msg = "Meow" + get_emoji(":cat:")
         await message.channel.send(msg)
-        
-        
