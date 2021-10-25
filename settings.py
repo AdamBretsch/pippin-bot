b1521984ef4c8a83067b26bb98fa8eb9c50c9be1
AdamBretsch <31545748+AdamBretsch@users.noreply.github.com>
2021-10-24 23:00:54 -0500

    Update settings.py
    removed token

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ import os
 COMMAND_PREFIX = "~"
 
 # The bot token. Keep this secret!
-BOT_TOKEN = "ODE1MzM2MDkwOTI4MDIxNTE0.YDq66A.HUDTWVPfceMvROWkQZHUtbk5XTU"
+BOT_TOKEN = "secret"
 
 # The now playing game. Set this to anything false-y ("", None) to disable it
 NOW_PLAYING = COMMAND_PREFIX + "meow"
